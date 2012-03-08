67470730c3349669b95bcae670d9b8b629d25795
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-08 22:55:08 +0100

    Multiple connections, like a boss

--- weechat.js
+++ weechat.js
@@ -3,7 +3,7 @@ events = require('events'),
 protocol = require('./protocol.js'),
 color = require('./color.js');
 
-var client, id = 0,
+var id = 0,
 em = new events.EventEmitter(),
 self = this;
 
@@ -27,7 +27,16 @@ exports.style = function(line) {
 };
 
 exports.connect = function(port, host, password, cb) {
-    function connect() {
+    if (arguments.length === 3) {
+        cb = password;
+        password = host;
+    }
+    return new WeeChat(port, host, password, cb);
+};
+
+function WeeChat(port, host, password, cb) {
+    if (! (this instanceof WeeChat)) return new WeeChat(port, host, password, cb);
+    var client = net.connect(port, host, password, function connect() {
         var err = false;
         self.write('init password=' + password + ',compression=off');
         // Ping test password 
@@ -45,134 +54,125 @@ exports.connect = function(port, host, password, cb) {
             }
         },
         100);
-    }
-
-    if (arguments.length === 4) {
-        client = net.connect(port, host, connect);
-    } else {
-        cb = password;
-        password = host;
-        client = net.connect(port, connect);
-    }
-
+    });
     client.on('error', function(err) {
         cb(err);
     });
-};
 
-exports.on = function(listener, cb) {
-    if (arguments.length === 1) {
-        cb = listener;
-        listener = '*';
-    }
+    this.on = function(listener, cb) {
+        if (arguments.length === 1) {
+            cb = listener;
+            listener = '*';
+        }
 
-    em.on(listener, cb);
-    if (aliases[listener]) {
-        em.on(aliases[listener], cb);
-    }
-};
+        em.on(listener, cb);
+        if (aliases[listener]) {
+            em.on(aliases[listener], cb);
+        }
+    };
 
-exports.write = function(msg, cb) {
-    id++;
-    if (cb) em.once(id, cb);
-    client.write('(' + id + ') ' + msg + '\n');
-};
+    this.write = function(msg, cb) {
+        id++;
+        if (cb) em.once(id, cb);
+        client.write('(' + id + ') ' + msg + '\n');
+    };
 
-exports.version = function(cb) {
-    if (cb) {
-        self.write('info version', function(v) {
-            cb(v[0].value);
-        });
-    }
-};
-
-exports.buffers = function(cb) {
-    if (cb) {
-        self.write(getbuffers, function(buffers) {
-            buffers = buffers.map(function(buffer) {
-                var lv = buffer.local_variables;
-                return {
-                    id: buffer.pointers[0],
-                    number: buffer.number,
-                    fullName: buffer.full_name,
-                    typeId: buffer.type,
-                    title: buffer.title,
-                    plugin: lv.plugin,
-                    channel: lv.channel,
-                    nick: lv.nick,
-                    type: lv.type,
-                    name: lv.name
-                };
+    this.version = function(cb) {
+        if (cb) {
+            self.write('info version', function(v) {
+                cb(v[0].value);
             });
-            cb(buffers);
-        });
-    }
-};
+        }
+    };
 
-exports.lines = function(bufferid, cb) {
-    if (arguments.length === 1) {
-        cb = bufferid;
-        bufferid = 'gui_buffers(*)';
-    }
-    if (cb) {
-        self.write(getlines1 + bufferid + getlines2, function(lines) {
-            lines = lines.map(function(line) {
-                return {
-                    buffer: '0x' + line.pointers[0],
-                    prefix: line.prefix,
-                    date: line.date,
-                    displayed: line.displayed,
-                    message: line.message
-                };
-            });
-            cb(lines);
-        });
-    }
-};
-
-exports.bufferlines = function(cb) {
-    if (cb) {
-        self.lines(function(lines) {
-            self.buffers(function(buffers) {
-                buffers.forEach(function(buffer) {
-                    if (!buffer.lines) {
-                        buffer.lines = [];
-                    }
-                    lines.filter(function(line) {
-                        return line.buffer.match(buffer.id);
-                    }).forEach(function(line) {
-                        buffer.lines.push(line);
-                    });
+    this.buffers = function(cb) {
+        if (cb) {
+            self.write(getbuffers, function(buffers) {
+                buffers = buffers.map(function(buffer) {
+                    var lv = buffer.local_variables;
+                    return {
+                        id: buffer.pointers[0],
+                        number: buffer.number,
+                        fullName: buffer.full_name,
+                        typeId: buffer.type,
+                        title: buffer.title,
+                        plugin: lv.plugin,
+                        channel: lv.channel,
+                        nick: lv.nick,
+                        type: lv.type,
+                        name: lv.name
+                    };
                 });
                 cb(buffers);
             });
+        }
+    };
+
+    this.lines = function(bufferid, cb) {
+        if (arguments.length === 1) {
+            cb = bufferid;
+            bufferid = 'gui_buffers(*)';
+        }
+        if (cb) {
+            self.write(getlines1 + bufferid + getlines2, function(lines) {
+                lines = lines.map(function(line) {
+                    return {
+                        buffer: '0x' + line.pointers[0],
+                        prefix: line.prefix,
+                        date: line.date,
+                        displayed: line.displayed,
+                        message: line.message
+                    };
+                });
+                cb(lines);
+            });
+        }
+    };
+
+    this.bufferlines = function(cb) {
+        if (cb) {
+            self.lines(function(lines) {
+                self.buffers(function(buffers) {
+                    buffers.forEach(function(buffer) {
+                        if (!buffer.lines) {
+                            buffer.lines = [];
+                        }
+                        lines.filter(function(line) {
+                            return line.buffer.match(buffer.id);
+                        }).forEach(function(line) {
+                            buffer.lines.push(line);
+                        });
+                    });
+                    cb(buffers);
+                });
+            });
+        }
+    };
+
+    function onData(data) {
+        protocol.data(data, function(id, obj) {
+            if (!id) id = '';
+
+            [id, '*'].forEach(function(l) {
+                if (!Array.isArray(obj)) obj = [obj];
+
+                obj = obj.map(function(o) {
+                    if (o.pointers) {
+                        o.pointers = o.pointers.map(function(p) {
+                            if (!p.match(/^0x/)) {
+                                return '0x' + p;
+                            }
+                            return p;
+                        });
+                    }
+                    if (o.buffer && ! o.buffer.match(/^0x/)) {
+                        o.buffer = '0x' + o.buffer;
+                    }
+                    return o;
+                });
+                em.emit(l, obj, id);
+            });
         });
     }
-};
-
-function onData(data) {
-    protocol.data(data, function(id, obj) {
-        if (!id) id = '';
-
-        [id, '*'].forEach(function(l) {
-            if (!Array.isArray(obj)) obj = [obj];
-
-            obj = obj.map(function(o) {
-                if (o.pointers) {
-                    o.pointers = o.pointers.map(function(p) {
-                        if (!p.match(/^0x/)) {
-                            return '0x' + p;
-                        }
-                        return p;
-                    });
-                }
-                if (o.buffer && ! o.buffer.match(/^0x/)) {
-                    o.buffer = '0x' + o.buffer;
-                }
-                return o;
-            });
-            em.emit(l, obj, id);
-        });
-    });
 }
 
