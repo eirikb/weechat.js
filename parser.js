4c62a9feeb502670868882e8327ff61e08e73cbc
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-20 07:51:00 +0100

    Handling concurrence issues on incoming buffer by piling it up

--- parser.js
+++ parser.js
@@ -5,7 +5,8 @@ exports.Parser = function Parser(cb) {
     if (! (this instanceof Parser)) return new Parser(cb);
 
     var data, self = this,
-    total = 0;
+    total = 0,
+    unzipping = false;
 
     function parseData(data, cb) {
         protocol.setData(data);
@@ -16,42 +17,54 @@ exports.Parser = function Parser(cb) {
         total = 0;
     }
 
+    function gotParts(data) {
+        protocol.setData(data);
+        // Remove total from data
+        protocol.getInt();
+        compression = protocol.getChar();
+        data = protocol.getData();
+
+        if (compression) {
+            unzipping = true;
+            zlib.unzip(data, function(err, data) {
+                unzipping = false;
+                if (err) throw err;
+                parseData(data, cb);
+            });
+        } else {
+            parseData(data, cb);
+        }
+    }
+
+    function concatBuffers(bufferA, bufferB) {
+        var buffer = new Buffer(bufferA.length + bufferB.length);
+        bufferA.copy(buffer);
+        bufferB.copy(buffer, bufferA.length);
+        return buffer;
+    }
+
     this.onData = function(part) {
         var tmp, compression;
 
-        if (total === 0) {
+        if (unzipping || total !== 0) {
+            concatBuffers(data, part);
+        } else {
             data = part;
             protocol.setData(data);
             total = protocol.getInt();
-        } else {
-            tmp = new Buffer(data.length + part.length);
-            data.copy(tmp);
-            part.copy(tmp, data.length);
-            data = tmp;
-        }
 
-        if (data.length >= total) {
-            protocol.setData(data);
-            // Remove total from data
-            protocol.getInt();
-            compression = protocol.getChar();
-            data = protocol.getData();
+            if (data.length >= total) {
+                tmp = null;
+                if (data.length > total) {
+                    tmp = data.slice(0, total);
+                    data = data.slice(total);
+                }
 
-            tmp = null;
-            if (data.length > total) {
-                tmp = data.slice(0, total);
-                data = data.slice(total);
-            }
-            if (compression) {
-                zlib.unzip(data, function(err, data) {
-                    if (err) throw err;
-                    parseData(data, cb);
-                });
-            } else {
-                parseData(data, cb);
-            }
-            if (tmp) {
-                self.onData(tmp);
+                gotParts(data);
+
+                if (tmp) {
+                    self.onData(tmp);
+                }
             }
         }
     };
