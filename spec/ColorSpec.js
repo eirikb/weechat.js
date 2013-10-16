31de01097b97cf8a6f4802e427342d77fd29cce6
eirikb <eirikb@eirikb.no>
2013-10-16 19:35:17 +0200

    Update color from glowing-bear (@torhve)

--- spec/ColorSpec.js
+++ spec/ColorSpec.js
@@ -3,7 +3,7 @@
 var data = '\u0019F00 default\u0019F01 black\u0019F02 darkgray\u0019F03 red\u0019F04 lightred\u0019F05 green\u0019F06 lightgreen\u0019F07 brown\u0019F08 yellow\u0019F09 blue \u0019F10 lightblue\u0019F11 magenta\u0019F12 lightmagenta\u0019F13 cyan\u0019F14 lightcyan\u0019F15 gray\u0019F16 white \u0019F13 cyan\u0019F11 magenta\u0019F05 green\u0019F07 brown\u0019F10 lightblue\u0019F00 default\u0019F14 lightcyan\u0019F12 lightmagenta\u0019F06 lightgreen \u0019F09 blue';
 
 var colorMap = {
-  'default': '#000000',
+  'default': '#666666',
   black: '#AA0000',
   darkgray: '#00AA00',
   red: '#AA5500',
@@ -19,22 +19,22 @@ var colorMap = {
   cyan: '#FF55FF',
   lightcyan: '#55FFFF',
   gray: '#FFFFFF',
-  white: '#000000'
+  white: '#666666'
 };
 
 describe('Color', function() {
   it('should have X "parts"', function() {
-    expect(weechat.color(data).length).toEqual(27);
+    expect(weeChat.color(data).length).toEqual(27);
   });
 
   it('should have no set background', function() {
-    weechat.color(data).forEach(function(c) {
+    weeChat.color(data).forEach(function(c) {
       expect(c.bg).toBeUndefined();
     });
   });
 
   it('should find every color in colorMap', function() {
-    weechat.color(data).forEach(function(c) {
+    weeChat.color(data).forEach(function(c) {
       var text = c.text.trim();
       expect(colorMap[text]).toEqual(c.fg);
     });
